7710c8148d903c49f5e4b4874e65e4d83e12be22
施霈帆 <andynoway253@gmail.com>
2023-09-08 15:43:24 +0800

    新增mp3 module

--- src/app/2019/stage3/mp3player.module.ts
+++ src/app/2019/stage3/mp3player.module.ts
@@ -2,19 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
-import { AlbumComponent } from './album/album.component';
-import { ArtistComponent } from './artist/artist.component';
+
 import { MP3PlayerComponent } from './mp3player.component';
-import { SongComponent } from './song/song.component';
+import { RouterModule } from '@angular/router';
+import { MP3PlayerRoutingModule } from './mp3player-routing.module';
+import { NbLayoutModule } from '@nebular/theme';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, FlexLayoutModule],
-  declarations: [
-    MP3PlayerComponent,
-    AlbumComponent,
-    ArtistComponent,
-    SongComponent,
+  imports: [
+    CommonModule,
+    FormsModule,
+    FlexLayoutModule,
+    RouterModule,
+
+    NbLayoutModule,
+
+    MP3PlayerRoutingModule,
   ],
-  exports: [MP3PlayerComponent],
+  declarations: [MP3PlayerComponent],
 })
 export class MP3PlayerModule {}
