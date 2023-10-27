c49b4878ec397c02217e2ff5cac5eb912aa91dc4
施霈帆 <andynoway253@gmail.com>
2023-10-27 17:35:34 +0800

    save

--- src/app/2019/stage4/payment.component.ts
+++ src/app/2019/stage4/payment.component.ts
@@ -1,13 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  FormBuilder,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { NbToastrService, NbStepperComponent } from '@nebular/theme';
 
 @Component({
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss'],
 })
 export class PaymentComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private fb: FormBuilder,
 
-  activeOption: string = '';
+    private toastrService: NbToastrService
+  ) {}
+
+  @ViewChild('stepper') stepper: NbStepperComponent;
+
+  get cardNumber() {
+    return this.cardForm.get('cardNumber');
+  }
+
+  identityRevealedValidator: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    return control.get('number1')?.invalid ||
+      control.get('number2')?.invalid ||
+      control.get('number3')?.invalid ||
+      control.get('number4')?.invalid
+      ? { identityRevealed: true }
+      : null;
+  };
+
+  activeOption = '';
+
+  visaUrl = 'assets/image/stage4/visa.svg';
+
+  jcbUrl = 'assets/image/stage4/jcb.svg';
+
+  masterCardUrl = 'assets/image/stage4/mastercard.svg';
+
+  checked = false;
 
   cards = [
     { name: '信用卡/金融卡', image: 'assets/image/stage4/credit-card.svg' },
@@ -17,7 +55,88 @@ export class PaymentComponent implements OnInit {
     { name: 'ATM 轉帳', image: 'assets/image/stage4/atm.svg' },
   ];
 
-  ngOnInit(): void {}
+  months = Array(13)
+    .fill(1)
+    .map((_, i) => i);
+
+  years = Array(25)
+    .fill(new Date().getFullYear())
+    .map((x, i) => x + i);
+
+  cardForm: FormGroup = this.fb.group({
+    radio: ['full'],
+    cardNumber: this.fb.group(
+      {
+        number1: ['1111', [Validators.required, Validators.minLength(4)]],
+        number2: ['2222', [Validators.required, Validators.minLength(4)]],
+        number3: ['3333', [Validators.required, Validators.minLength(4)]],
+        number4: ['4444', [Validators.required, Validators.minLength(4)]],
+      },
+      { validators: this.identityRevealedValidator }
+    ),
+    month: [1, [Validators.required]],
+    year: [2023, [Validators.required]],
+    cvc: ['123', [Validators.required, Validators.minLength(3)]],
+    email: ['andynoway@gmial.com', [Validators.required, Validators.email]],
+    check: [false, [Validators.requiredTrue]],
+  });
+
+  // shopForm: FormGroup = this.fb.group({
+  //   shop: [''],
+  //   email: ['andynoway@gmial.com', [Validators.required, Validators.email]],
+  //   check: [false, [Validators.requiredTrue]],
+  // });
+
+  // WebForm: FormGroup = this.fb.group({
+  //   radio: ['full'],
+  //   cardNumber: this.fb.group(
+  //     {
+  //       number1: ['1111', [Validators.required, Validators.minLength(4)]],
+  //       number2: ['2222', [Validators.required, Validators.minLength(4)]],
+  //       number3: ['3333', [Validators.required, Validators.minLength(4)]],
+  //       number4: ['4444', [Validators.required, Validators.minLength(4)]],
+  //     },
+  //     { validators: this.identityRevealedValidator }
+  //   ),
+  //   month: [1, [Validators.required]],
+  //   year: [2023, [Validators.required]],
+  //   cvc: ['123', [Validators.required, Validators.minLength(3)]],
+  //   email: ['andynoway@gmial.com', [Validators.required, Validators.email]],
+  //   check: [false, [Validators.requiredTrue]],
+  // });
+
+  ngOnInit(): void {
+    this.cardNumber?.valueChanges.subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+    });
+    // this.cardNumber?.get('number1')?.valueChanges.subscribe({
+    //   next: (res) => {
+    //     if (this.cardNumber?.get('number1')?.valid) {
+    //       const first = res.charAt(0);
+
+    //       //  JCB
+    //       this.jcbUrl =
+    //         first === '3'
+    //           ? 'assets/image/stage4/jcb2.svg'
+    //           : 'assets/image/stage4/jcb.svg';
+
+    //       //  Visa
+    //       this.visaUrl =
+    //         first === '4'
+    //           ? 'assets/image/stage4/visa2.svg'
+    //           : 'assets/image/stage4/visa.svg';
+
+    //       //  萬事達卡
+    //       this.masterCardUrl =
+    //         first === '5'
+    //           ? 'assets/image/stage4/mastercard2.svg'
+    //           : 'assets/image/stage4/mastercard.svg';
+    //     }
+    //   },
+    // });
+  }
 
   setActive(option: string) {
     this.activeOption = option;
@@ -25,5 +144,20 @@ export class PaymentComponent implements OnInit {
 
   isActive(option: string) {
     return this.activeOption === option;
+  }
+
+  toggle(e: boolean) {
+    this.checked = e;
+  }
+
+  checkPayType() {
+    if (!this.activeOption) {
+      this.toastrService.warning('請先選擇付款方式', '警告', {
+        preventDuplicates: true,
+      });
+      return;
+    }
+
+    this.stepper.next();
   }
 }
--- src/app/2019/stage4/payment.module.ts
+++ src/app/2019/stage4/payment.module.ts
@@ -3,23 +3,36 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {
   NbButtonModule,
+  NbCheckboxModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
   NbStepperModule,
+  NbToastrModule,
   NbTooltipModule,
 } from '@nebular/theme';
 import { PaymentComponent } from './payment.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NumberDirective } from './directive/numbers-only.directive';
+import { SanitizeUrlPipe } from './pipe/sanitize-url';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     FlexLayoutModule,
+    ReactiveFormsModule,
 
     NbButtonModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbInputModule,
+    NbSelectModule,
     NbStepperModule,
     NbTooltipModule,
+    NbToastrModule.forRoot(),
   ],
-  declarations: [PaymentComponent],
-  exports: [PaymentComponent],
+  declarations: [PaymentComponent, NumberDirective, SanitizeUrlPipe],
+  exports: [PaymentComponent, NumberDirective, SanitizeUrlPipe],
 })
 export class PaymentModule {}
