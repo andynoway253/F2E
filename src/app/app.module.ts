d7ad37866ffd732a82a1df49b11c9272b0af03e5
施霈帆 <andynoway253@gmail.com>
2023-11-10 11:46:35 +0800

    刪除不必要的引入

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListModule } from './todoList/todo-list.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import {
   NbButtonModule,
-  NbCardModule,
   NbIconModule,
   NbLayoutModule,
-  NbListModule,
   NbMenuModule,
-  NbSidebarModule,
   NbThemeModule,
 } from '@nebular/theme';
 import { PompdoroModule } from './2019/stage1/pompdoro.module';
 import { MP3PlayerModule } from './2019/stage3/mp3player.module';
-import { PaymentComponent } from './2019/stage4/payment.component';
 import { PaymentModule } from './2019/stage4/payment.module';
 import { MaskMapModule } from './2019/stage10/mask-map.module';
 
@@ -31,16 +25,10 @@ import { MaskMapModule } from './2019/stage10/mask-map.module';
 
     NbThemeModule.forRoot(),
     NbMenuModule.forRoot(),
-    NbSidebarModule.forRoot(),
     NbLayoutModule,
     NbButtonModule,
-    NbCardModule,
     NbIconModule,
-    NbListModule,
 
-    FlexLayoutModule,
-
-    TodoListModule,
     PompdoroModule,
     MP3PlayerModule,
     PaymentModule,
