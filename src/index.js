12ec9d64712969515780ada2a98be54eacb4bf9b
andynoway253 <andynoway253@gmail.com>
2023-11-27 02:04:11 +0800

    後端服務

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("add-message", (message) => {
+    console.log("add message");
+    io.emit("message", { type: "new-message", text: message });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("server running at http://localhost:3000");
+});
