caa41c92158ae77137aa038c318bb5f859e8f43b
andynoway253 <andynoway253@gmail.com>
2023-12-21 02:58:55 +0800

    更改url

--- frontend/src/app/2019/stage7/chatroom.service.ts
+++ frontend/src/app/2019/stage7/chatroom.service.ts
@@ -6,9 +6,9 @@ import { io } from 'socket.io-client';
   providedIn: 'root',
 })
 export class ChatService {
-  // private apiUrl = 'https://f2e.onrender.com'; // 你的Node.js服务器地址
+  private apiUrl = 'https://f2e.onrender.com'; // 你的Node.js服务器地址
 
-  private apiUrl = 'http://localhost:3000'; // 你的Node.js服务器地址
+  // private apiUrl = 'http://localhost:3000'; // 你的Node.js服务器地址
   private socket = io(this.apiUrl, { withCredentials: true });
 
   sendMessage(message: { userName: string; text: string }) {
